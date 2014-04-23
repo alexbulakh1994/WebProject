be01d3145fdb12dbda09687645c233ef14a52f09
alexbulakh1994 <alexbulakh707@gmail.com>
2014-04-23 22:21:36 +0300

    add my Exception

---
+++ src/Restoran_Struct/NotFindDishException.java
@@ -0,0 +1,17 @@
+package Restoran_Struct;
+
+public class NotFindDishException extends Exception{
+	
+	  //Parameterless Constructor
+    public NotFindDishException() {}
+
+    //Constructor that accepts a message
+    public NotFindDishException(String message)
+    {
+       super(message);
+    }
+    
+    //usage
+    
+
+}
