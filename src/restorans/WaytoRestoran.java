e435e41421b95d25b588da25b11fb4644ce25f51
alexbulakh1994 <alexbulakh707@gmail.com>
2014-04-28 09:57:34 +0300

    Update and rename Way_to_Restoran.java to WaytoRestoran.java

--- src/restorans/WaytoRestoran.java
+++ src/restorans/WaytoRestoran.java
@@ -2,7 +2,7 @@ package Restoran_Struct;
 
 public class WaytoRestoran extends Info{
 
-	public Way_to_Restoran(String address, double mark, String name, Coordinate coordinate) {
+	public WaytoRestoran(String address, double mark, String name, Coordinate coordinate) {
 		super(address, mark, name, coordinate);
 	}
 	
